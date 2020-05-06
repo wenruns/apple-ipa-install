8a994f505cc50b71bc220655c5f903995a0cf378
hww <hww@8kqw.com>
2020-05-06 15:54:32 +0800

    no message

---
+++ src/MobileConfig.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2020/5/6
+ * Time: 15:49
+ */
+
+namespace wenruns\apple\ipa\install;
+
+
+class MobileConfigController
+{
+
+}
---
+++ src/MobileProvision.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2020/5/6
+ * Time: 15:52
+ */
+
+namespace wenruns\apple\ipa\install;
+
+
+class MobileVersion
+{
+
+}
